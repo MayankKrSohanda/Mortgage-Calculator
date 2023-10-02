fbeb2251eed15db448362c903aa0a614a1b8e7f5
Mayank Kumar Sohanda <ms4959@srmist.edu.in>
2023-10-02 21:12:14 +0530

    Fine tuning.

--- src/MortgageCalculator.java
+++ src/MortgageCalculator.java
@@ -1,6 +1,6 @@
 public class MortgageCalculator {
-    public final static byte MONTHS_IN_YEAR = 12;
-    public final static byte PERCENT = 100;
+    private final static byte MONTHS_IN_YEAR = 12;
+    private final static byte PERCENT = 100;
 
     private final int principal;
     private final float annualInterest;
@@ -14,7 +14,7 @@ public class MortgageCalculator {
 
     public double calculateBalance(short numberOfPaymentsMade) {
         float monthlyInterest = getMonthlyInterest();
-        short numberOfPayments = (short) getNumberOfPayments();
+        short numberOfPayments = getNumberOfPayments();
 
         return principal
                 * (Math.pow(1 + monthlyInterest, numberOfPayments) - Math.pow(1 + monthlyInterest, numberOfPaymentsMade))
@@ -24,19 +24,23 @@ public class MortgageCalculator {
 
     public double calculateMortgage() {
         float monthlyInterest = getMonthlyInterest();
-        short numberOfPayments = (short) getNumberOfPayments();
+        short numberOfPayments = getNumberOfPayments();
 
         return principal
                 * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments)
                 / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1));
     }
 
-    public byte getYears() {
-        return years;
+    public double[] getRemainingBalances() {
+        double[] balances = new double[getNumberOfPayments()];
+        for (short month = 1; month <= balances.length; month++) {
+            balances[month - 1] = calculateBalance(month);
+        }
+        return balances;
     }
 
-    private int getNumberOfPayments() {
-        return years * MONTHS_IN_YEAR;
+    private short getNumberOfPayments() {
+        return (short) (years * MONTHS_IN_YEAR);
     }
 
     private float getMonthlyInterest() {
